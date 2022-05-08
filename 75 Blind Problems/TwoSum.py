36c3609ce85b573aa4ff9c3921cdf918bd355992
Vatsal Yadav <vt769414@dal.ca>
2022-05-08 10:37:30 -0300

    Two Sum Problem Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ 75 Blind Problems/TwoSum.py
@@ -0,0 +1,23 @@
+# LeetCode link: https://leetcode.com/problems/two-sum/submissions/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numDict = {}
+        count = 0
+        for n in nums:
+            potentialNum = target - n
+            if potentialNum in numDict:
+                return [numDict[potentialNum], count]
+            else:
+                numDict[n] = count
+            count += 1
+        return []
+
+
+solutionObj = Solution()
+print(solutionObj.twoSum([2, 7, 11, 15], 9))
