08687ed02ed0d3c091b66a687d97f6a3e1ae593c
Vatsal Yadav <vatsalyad15@gmail.com>
2022-05-09 05:23:28 -0300

    Sliding Window solution for Best Time to Buy and Sell Stock

--- 75 Blind Problems/BuySellStocks.py
+++ 75 Blind Problems/BuySellStocks.py
@@ -6,7 +6,7 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-
+        # Time = O(n^2)
         if len(prices) < 2:
             return 0
 
@@ -16,6 +16,28 @@ class Solution(object):
                 if prices[j] - prices[i] > maxDiff:
                     maxDiff = prices[j] - prices[i]
         return maxDiff
+
+    def maxProfitSlidingWindow(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # Time = O(n)
+        b = prices[0]
+        s = prices[0]
+        maxProfit = 0
+        i = 0
+        while i < len(prices):
+            if prices[i] < b:
+                b = prices[i]
+                s = prices[i]
+            elif prices[i] > s:
+                s = prices[i]
+            if s - b > maxProfit:
+                maxProfit = s - b
+            i += 1
+        return maxProfit
+
 
 solutionObj = Solution()
 print(solutionObj.maxProfitBruteForce([7,1,5,3,6,4]))
