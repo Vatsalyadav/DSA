8aaf536a425eb65f1222118366ba41e24e23bdcc
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-08 17:38:43 -0300

    Time: 229 ms (5.00%), Space: 37 MB (5.15%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -13,7 +13,6 @@ class Solution(object):
         if root is None:
             return 0
         maxTree = self.createMaxTree(root)
-        
         return self.inorderTravers(maxTree)
     
     def createMaxTree(self, root, rev = None):
@@ -22,20 +21,11 @@ class Solution(object):
         rev = TreeNode([root.val,root.val])
         rev.right = self.createMaxTree(root.right, rev.right)
         rev.left = self.createMaxTree(root.left, rev.left)
-#       Set value as infinity instead of 0 otherwise max will take 0 instead of negative numbers
         left = rev.left if rev.left is not None else TreeNode([0,0])
         right = rev.right if rev.right is not None else TreeNode([0,0])
         
         rev.val = [max(rev.val[0], rev.val[0] + left.val[1] + right.val[1], rev.val[0] + left.val[1], rev.val[0] + right.val[1]),max(rev.val[0], rev.val[0] + left.val[1], rev.val[0] + right.val[1])]
         return rev
-        
-    # def findMaxValue(self, maxTree):
-    #     # print(maxTree)
-    #     if maxTree is None:
-    #         return float("-inf")
-    #     left = maxTree.left.val if maxTree.left is not None else float("-inf")
-    #     right = maxTree.right.val if maxTree.right is not None else float("-inf")
-    #     return max(maxTree.val, left, right, self.findMaxValue(maxTree.left), self.findMaxValue(maxTree.right))
     
     def inorderTravers(self, maxTree, maxSum = float("-inf")):
         if maxTree is None:
