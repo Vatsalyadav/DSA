a297b7e3ab062534e0f0b96388a2f5b96155b709
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-08 17:53:33 -0300

    Time: 275 ms (5.00%), Space: 36.9 MB (5.15%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,6 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    
     def maxPathSum(self, root):
         """
         :type root: TreeNode
@@ -13,7 +14,8 @@ class Solution(object):
         if root is None:
             return 0
         maxTree = self.createMaxTree(root)
-        return self.inorderTravers(maxTree)
+        
+        return self.inorderTraverse(maxTree)
     
     def createMaxTree(self, root, rev = None):
         if root is None:
@@ -21,15 +23,24 @@ class Solution(object):
         rev = TreeNode([root.val,root.val])
         rev.right = self.createMaxTree(root.right, rev.right)
         rev.left = self.createMaxTree(root.left, rev.left)
+#       Set value as infinity instead of 0 otherwise max will take 0 instead of negative numbers
         left = rev.left if rev.left is not None else TreeNode([0,0])
         right = rev.right if rev.right is not None else TreeNode([0,0])
         
         rev.val = [max(rev.val[0], rev.val[0] + left.val[1] + right.val[1], rev.val[0] + left.val[1], rev.val[0] + right.val[1]),max(rev.val[0], rev.val[0] + left.val[1], rev.val[0] + right.val[1])]
         return rev
+        
+    # def findMaxValue(self, maxTree):
+    #     # print(maxTree)
+    #     if maxTree is None:
+    #         return float("-inf")
+    #     left = maxTree.left.val if maxTree.left is not None else float("-inf")
+    #     right = maxTree.right.val if maxTree.right is not None else float("-inf")
+    #     return max(maxTree.val, left, right, self.findMaxValue(maxTree.left), self.findMaxValue(maxTree.right))
     
-    def inorderTravers(self, maxTree, maxSum = float("-inf")):
+    def inorderTraverse(self, maxTree, maxSum = float("-inf")):
         if maxTree is None:
             return None
         maxSum = max(maxTree.val)
-        return max(maxSum, self.inorderTravers(maxTree.left, maxSum), self.inorderTravers(maxTree.right, maxSum))
+        return max(maxSum, self.inorderTraverse(maxTree.left, maxSum), self.inorderTraverse(maxTree.right, maxSum))
         
