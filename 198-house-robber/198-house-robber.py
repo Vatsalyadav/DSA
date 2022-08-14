2c198afddb48e779487a5e7487fe9e8846eff214
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-14 13:02:01 -0300

    Time: 39 ms (14.71%), Space: 13.4 MB (42.79%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l = len(nums)
-        
-        if l < 3:
+        if len(nums) < 3:
             return max(nums)
-        
-        if l == 3:
-            return max(nums[1], nums[0]+nums[2])
-        
-#         grid = [maxSumButAdjacentToNext, maxSumButNotAdjacentToNext, maxButNotAdj, maxButAdj ]
         grid = [max(nums[1], nums[0]+nums[2]), nums[1], max(nums[:2]), max(nums[:3])]
         index = 3
         while index < len(nums):
