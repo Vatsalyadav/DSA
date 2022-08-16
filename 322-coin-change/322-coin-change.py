3b3766f4cacb6fc39fb4c71265ca8d6d040435f0
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-16 00:48:50 -0300

    Time: 925 ms (89.93%), Space: 13.9 MB (83.39%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        
+        if amount == 0:
+            return 0
+        
+        ans = [float("inf")] * (amount+1)
+        ans[0] = 0
+        for coin in coins:
+            for index in range(coin, amount+1):
+                coinsRequired = 1 + ans[index - coin]
+                if coinsRequired < ans[index]:
+                    ans[index] = coinsRequired
+        return ans[-1] if ans[-1]!=float("inf") else -1
