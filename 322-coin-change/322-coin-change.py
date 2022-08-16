91ac3a93f5c38c920c956ce6b651b69d0f8f5234
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-16 00:53:08 -0300

    Time: 2618 ms (11.18%), Space: 14 MB (70.07%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -6,9 +6,16 @@ class Solution(object):
         :rtype: int
         """
         
+        if amount == 0:
+            return 0
+        
         ans = [float("inf")] * (amount+1)
         ans[0] = 0
         for coin in coins:
             for index in range(coin, amount+1):
+                coinsRequired = 1 + ans[index - coin]
+                
+                if coinsRequired < ans[index]:
+                    ans[index] = coinsRequired
                 ans[index] = min(ans[index], 1 + ans[index - coin])
         return ans[-1] if ans[-1]!=float("inf") else -1
