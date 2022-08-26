266cd78acc1ea83212e7c1843cc3acc1a1778c72
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-26 19:51:01 -0300

    Time: 1333 ms (75.00%), Space: 25.4 MB (75.00%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -4,14 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = {}
-        def dfs(index, even):
-            if index >= len(nums):
-                return 0
-            if (index, even) in dp:
-                return dp[(index, even)]
-            val = nums[index] if even else (-1 * nums[index])
-            dp[(index, even)] = max(val + dfs(index+1, not even), dfs(index+1, even))
-            return dp[(index, even)]
         
-        return dfs(0, True)
+        sumOdd = sumEven = 0
+        for i in range(len(nums)-1, -1, -1):
+            tempEven = max(sumOdd + nums[i], sumEven)
+            tempOdd = max(sumEven - nums[i], sumOdd)
+            sumOdd, sumEven = tempOdd, tempEven
+        return sumEven
+            
+    
+#     Decision Tree Approach
+#         dp = {}
+#         def dfs(index, even):
+#             if index >= len(nums):
+#                 return 0
+#             if (index, even) in dp:
+#                 return dp[(index, even)]
+#             val = nums[index] if even else (-1 * nums[index])
+#             dp[(index, even)] = max(val + dfs(index+1, not even), dfs(index+1, even))
+#             return dp[(index, even)]
+        
+#         return dfs(0, True)
