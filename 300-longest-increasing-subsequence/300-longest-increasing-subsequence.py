047d7f8885b173b472a0c8721c0985bf7f63ea97
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-26 00:11:02 -0300

    Time: 2821 ms (54.30%), Space: 14 MB (15.35%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -4,18 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        memo = [1] * len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    memo[i] = max(memo[i], memo[j]+1)
+        return max(memo)
+# #       Unique Approach: Patience Sort
+#         seq = []
         
-        seq = []
-        
-        for num in nums:
-            pos = -1
-            for i in range(len(seq)):
-                if num <= seq[i]:
-                    pos = i
-                    break
-            if pos == -1:
-                seq.append(num)
-            else:
-                seq[pos] = num
-        return len(seq)
-            
+#         for num in nums:
+#             pos = -1
+#             for i in range(len(seq)):
+# #                 find left most number that is greater than new number, use binary search here
+#                 if num <= seq[i]:
+#                     pos = i
+#                     break
+#             if pos == -1:
+#                 seq.append(num)
+#             else:
+#                 seq[pos] = num
+#         return len(seq)
