7b34fe0751fbf0fff10f8a54d853b6599dfada17
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-27 00:37:38 -0300

    Time: 45 ms (89.32%), Space: 14.7 MB (21.59%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,14 +4,21 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        dp = {}
+        profit = 0
         
-        def dfs(index, even):
-            if index == len(prices):
-                return 0
-            if (index, even) in dp:
-                return dp[(index, even)]
-            currSum = prices[index] if even else (-1*prices[index])
-            dp[(index, even)] = max(currSum + dfs(index+1, not even), dfs(index+1, even))
-            return dp[(index, even)]
-        return dfs(0, False)
+        for i in range(1, len(prices)):
+            if prices[i-1] < prices[i]:
+                profit += prices[i] - prices[i-1]
+        return profit
+        
+#         dp = {}
+        
+#         def dfs(index, even):
+#             if index == len(prices):
+#                 return 0
+#             if (index, even) in dp:
+#                 return dp[(index, even)]
+#             currSum = prices[index] if even else (-1*prices[index])
+#             dp[(index, even)] = max(currSum + dfs(index+1, not even), dfs(index+1, even))
+#             return dp[(index, even)]
+#         return dfs(0, False)
