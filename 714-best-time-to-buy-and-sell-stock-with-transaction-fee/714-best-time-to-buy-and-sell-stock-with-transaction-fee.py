a0e5e7f89ff9f2424990122139ca3efbdc3f4cc6
Vatsalyadav <vatsalyad15@gmail.com>
2022-08-27 01:02:33 -0300

    Time: 681 ms (79.52%), Space: 19.3 MB (43.37%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -5,14 +5,24 @@ class Solution(object):
         :type fee: int
         :rtype: int
         """
-        dp = {}
         
-        def dfs(index, even):
-            if index == len(prices):
-                return 0
-            if (index, even) in dp:
-                return dp[(index, even)]
-            currSum = prices[index] if even else (-1*prices[index])
-            dp[(index, even)] = max(currSum + dfs(index+1, not even) - (fee/2.0), dfs(index+1, even))
-            return dp[(index, even)]
-        return int(dfs(0, False))
+        buy = [0] * len(prices)
+        sell = [0] * len(prices)
+        buy[0] = -prices[0]
+        
+        for i in range(1, len(prices)):
+            buy[i] = max(buy[i-1], sell[i-1] - prices[i] )
+            sell[i] = max(sell[i-1], buy[i-1] + prices[i] - fee)
+
+        return sell[-1]
+#         dp = {}
+        
+#         def dfs(index, even):
+#             if index == len(prices):
+#                 return 0
+#             if (index, even) in dp:
+#                 return dp[(index, even)]
+#             currSum = prices[index] if even else (-1*prices[index])
+#             dp[(index, even)] = max(currSum + dfs(index+1, not even) - (fee/2.0), dfs(index+1, even))
+#             return dp[(index, even)]
+#         return int(dfs(0, False))
