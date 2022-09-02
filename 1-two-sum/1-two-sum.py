43c6a296079ee2ba90790779eaad2f0dd82162ab
Vatsalyadav <vatsalyad15@gmail.com>
2022-09-02 01:53:24 -0300

    Time: 105 ms (58.87%), Space: 15.1 MB (49.67%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         d = {}
+        
         for index, num in enumerate(nums):
-            if (target - num) in d:
-                return [index, d[target - num]]
-            else:
+            potentialMatch = target - num
+            
+            if potentialMatch in d:
+                return [d[potentialMatch], index]
+            if num not in d:
                 d[num] = index
-        return False
+            
