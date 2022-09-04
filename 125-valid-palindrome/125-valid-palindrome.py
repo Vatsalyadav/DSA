e4b1db508daa08bad73cba8ecd2ce7e32bd3ceb5
Vatsalyadav <vatsalyad15@gmail.com>
2022-09-04 20:27:31 -0300

    Time: 174 ms (5.02%), Space: 14.4 MB (56.92%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,13 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = "".join(filter(str.isalnum, s)).lower()
-        # return new == new[::-1]
-        left = 0
-        right = len(new)-1
-        while left<right:
-            if new[left] != new[right]:
+        
+        l = 0
+        r = len(s) - 1
+        
+        while l <= r:
+            
+            while l <= r:
+                if (ord(s[l]) >= 48 and ord(s[l]) <= 57) or (ord(s[l]) >= 97 and ord(s[l]) <= 122) or (ord(s[l]) >= 65 and ord(s[l]) <= 90):
+                    break
+                else:
+                    l+=1
+        
+            while l <= r:
+                if (ord(s[r]) >= 48 and ord(s[r]) <= 57) or (ord(s[r]) >= 97 and ord(s[r]) <= 122) or (ord(s[r]) >= 65 and ord(s[r]) <= 90):
+                    break
+                else:
+                    r-=1
+            
+            if l <= r and s[l].lower() != s[r].lower():
                 return False
-            else:
-                left+=1
-                right-=1
+            l+=1
+            r-=1
         return True
