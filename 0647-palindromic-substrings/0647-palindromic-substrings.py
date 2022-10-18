1674128316b6c57d593986c5f722ac23bd718ae8
Vatsalyadav <vatsalyad15@gmail.com>
2022-10-18 08:48:54 -0300

    Time: 319 ms (45.81%), Space: 13.9 MB (75.54%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,7 +2,31 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
         for i in range(len(s)):
-            for j in range(i+1, len(s)+1):
-                if s[i:j] == s[i:j][::-1]:
-                    count+=1
+            count+= self.oddLenPalindromes(i, s) + self.evenLenPalindromes(i, s)
+        return count
+    
+    def oddLenPalindromes(self, i, s):
+        left = i
+        right = i
+        count = 0
+        while left>=0 and right<len(s):
+            if s[left] == s[right]:
+                count+=1
+                left-=1
+                right+=1
+            else:
+                break
+        return count
+    
+    def evenLenPalindromes(self, i, s):
+        left = i
+        right = i+1
+        count = 0
+        while left>=0 and right<len(s):
+            if s[left] == s[right]:
+                count+=1
+                left-=1
+                right+=1
+            else:
+                break
         return count
