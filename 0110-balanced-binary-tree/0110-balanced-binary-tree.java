beb3a199ec1e9175314d8034d9a837f47cb57248
Vatsalyadav <vatsalyad15@gmail.com>
2023-10-06 00:14:04 -0300

    Time: 0 ms (100.00%), Space: 43 MB (68.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -17,11 +17,11 @@ class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null)
             return true;
-        return Math.abs(isBalancedHelper(root.left) - isBalancedHelper(root.right)) <=1 && isBalanced(root.left) && isBalanced(root.right);
+        return Math.abs(height(root.left) - height(root.right)) <=1 && isBalanced(root.left) && isBalanced(root.right);
     }
-    public int isBalancedHelper(TreeNode root) {
+    public int height(TreeNode root) {
         if(root == null)
             return 0;
-        return 1+ Math.max(isBalancedHelper(root.left), isBalancedHelper(root.right));
+        return 1+ Math.max(height(root.left), height(root.right));
     }
 }
