840e0c7c8aaceaed1d42feb4d3707f68476d287e
Vatsalyadav <vatsalyad15@gmail.com>
2023-10-12 23:59:50 -0300

    Time: 22 ms (78.28%), Space: 53.9 MB (27.64%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,31 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        TreeMap<Double, ArrayList<Integer>> sortedMap = new TreeMap<>();
-        HashMap<Double, Boolean> uniqueDist = new HashMap<>();
-        Double dist;
-        int i = 0;
-        for(int[] point: points){
-            dist = (Math.pow(point[0], 2)) + (Math.pow(point[1], 2));
-            if(uniqueDist.containsKey(dist)){
-                sortedMap.get(dist).add(i);
-            }
-            else{
-                sortedMap.put(dist,new ArrayList<Integer>(Arrays.asList(i)));
-                uniqueDist.put(dist, true);
-            }
-            i++;
-        }
-        
-        int[][] ans = new int[k][2];
-        
-        i = 0;
-        for(Map.Entry<Double, ArrayList<Integer>> entry : sortedMap.entrySet()){
-            for(int j = 0; j < entry.getValue().size() && i<k; j++, i++)
-                ans[i] = points[entry.getValue().get(j)];
-            if(i==k)
-                break;
-        }
-        
-        return ans;
+        Arrays.sort(points, (p1, p2)-> (p1[0]*p1[0]+p1[1]*p1[1]) - (p2[0]*p2[0]+p2[1]*p2[1]));
+        return Arrays.copyOfRange(points, 0, k);
     }
 }
